cedd88e6a5e86e18739c51396e7c91ca15818040
deoism <deoism@gmail.com>
2018-02-07 10:20:45 -0500

    first commit superherotown

---
+++ src/superheroes.service.js
@@ -0,0 +1,12 @@
+const heroURL =
+  "https://raw.githubusercontent.com/jtomchak/super-heroes/master/superhero.json";
+
+const getHeroes = () => {
+  return fetch(heroURL);
+};
+
+
+
+const getHeroById = id => {};
+
+export { getHeroes, getHeroById};
